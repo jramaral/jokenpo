dfd9f389b09c5d23d7670ddffdf5b45d476b5c1b
jose.amaral <j.robertoamaral@gmail.com>
2020-10-09 16:30:38 -0400

    refatorado

--- jokenpo-api/Data/IRepository.cs
+++ jokenpo-api/Data/IRepository.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace jokenpo_api.Data
 {
-    public interface IRepository
-    {
-        void Add<T>(T entity) where T : class;
-        void Update<T>(T entity) where T : class;
-        void Delete<T>(T entity) where T : class;
-        Task<bool> SaveChangeAsync();
-        Task<Restaurant[]> GetAllRestaurant();
-        Task<List<Jogador>> GetAllGamers();
-        Task<List<Jogada>> GetAllJogada();
-        Task<List<Partida>> GetAllPartida();
+  public interface IRepository
+  {
+    void Add<T>(T entity) where T : class;
+    void Update<T>(T entity) where T : class;
+    void Delete<T>(T entity) where T : class;
+    Task<bool> SaveChangeAsync();
 
-        Task<Jogada> GetJogadaById(int jogadaId);
+    Task<List<Jogador>> GetAllGamers();
+    Task<List<Jogada>> GetAllJogada();
+    Task<List<Partida>> GetAllPartida();
 
-        Task<Jogador> GetJogadorById(int jogadorId);
+    Task<Jogada> GetJogadaById(int jogadaId);
 
-        string GetResultado(Partida partida);
-    }
+    Task<Jogador> GetJogadorById(int jogadorId);
+
+    string GetResultado(Partida partida);
+  }
 }
--- jokenpo-api/Model/AppDbContext.cs
+++ jokenpo-api/Model/AppDbContext.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace jokenpo_api.Model
 {
-    public class AppDbContext : DbContext
-    {
-        public DbSet<Restaurant> Restaurants { get; set; }
-        public DbSet<Jogador> Jogadores { get; set; }
-        public DbSet<Partida> Partidas { get; set; }
-        public DbSet<Jogada> Jogadas { get; set; }
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
-           
-        }
+  public class AppDbContext : DbContext
+  {
 
+    public DbSet<Jogador> Jogadores { get; set; }
+    public DbSet<Partida> Partidas { get; set; }
+    public DbSet<Jogada> Jogadas { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
 
     }
+
+
+  }
 }
